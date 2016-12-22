dd2c6de4862bf42547e6010dfe1768e12ab1af1e
stepforwardtech <jrstrayhorn@gmail.com>
2016-12-22 10:33:24 -0500

    the backend is working now

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,68 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post'")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(req.post);
+  });
+});
+
+router.get('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) { return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment) {
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment'")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
 module.exports = router;
