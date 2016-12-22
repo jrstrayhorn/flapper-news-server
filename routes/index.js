899aa7203e52877cb121966d7118ea645e5cbf79
stepforwardtech <jrstrayhorn@gmail.com>
2016-12-22 15:11:24 -0500

    securing endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+var passport = require('passport');
+var jwt = require('express-jwt');
+var config = require('../server-config');
+
+var auth = jwt({secret: config.secretKey, userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,7 +25,7 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST posts lol */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function(err, post) {
@@ -49,7 +55,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post) {
     if (err) { return next(err); }
 
@@ -57,7 +63,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
 
@@ -85,12 +91,46 @@ router.param('comment', function(req, res, next, id) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment) {
     if (err) { return next(err); }
 
     res.json(comment);
   });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function (err) {
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()});
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
